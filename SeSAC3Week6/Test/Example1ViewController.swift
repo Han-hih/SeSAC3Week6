31726036dd79b61b309fb629f0fad590cc25b5c4
한 <hih458@naver.com>
2023-08-22 20:30:22 +0900

    example1 코드로 오토레이아웃 잡기

--- SeSAC3Week6/Test/Example1ViewController.swift
+++ SeSAC3Week6/Test/Example1ViewController.swift
@@ -9,20 +9,78 @@ import UIKit
 
 class Example1ViewController: UIViewController {
 
-    let grayView = UIView()
+    let grayView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+
+        return view
+    }()
+    let titleTextField = {
+        let view = UITextField()
+        view.borderStyle = .none
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 1
+        view.placeholder = "제목을 입력해주세요"
+        view.textAlignment = .center
+        view.font = .boldSystemFont(ofSize: 15)
+        
+        return view
+    }()
     
+    let dateTextField = {
+        let view = UITextField()
+        view.borderStyle = .none
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 1
+        view.placeholder = "날짜를 입력해주세요"
+        view.textAlignment = .center
+        view.font = .boldSystemFont(ofSize: 15)
+        
+        return view
+    }()
+    
+    let emptyTextField = {
+        let view = UITextField()
+        view.borderStyle = .none
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 1
+        
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
-        grayView.backgroundColor = .gray
         view.addSubview(grayView)
+        view.addSubview(titleTextField)
+        view.addSubview(dateTextField)
+        view.addSubview(emptyTextField)
+        
+        view.backgroundColor = .white
+        
         grayView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.equalTo(20)
             make.trailing.equalTo(-20)
             make.height.equalTo(200)
+        }
+        
+        titleTextField.snp.makeConstraints { make in
+            make.top.equalTo(grayView.snp_bottomMargin).offset(30)
+            make.horizontalEdges.equalTo(grayView)
+            make.height.equalTo(50)
+        }
+        
+        dateTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleTextField.snp_bottomMargin).offset(30)
+            make.horizontalEdges.equalTo(grayView)
+            make.height.equalTo(50)
+        }
+        
+        emptyTextField.snp.makeConstraints { make in
+            make.top.equalTo(dateTextField.snp_bottomMargin).offset(30)
+            make.horizontalEdges.equalTo(grayView)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
         
         
